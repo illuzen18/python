5337d8e968b39fb98bdec8a719dda7debf099f11
illuzen18 <103988058+illuzen18@users.noreply.github.com>
2025-01-03 17:19:45 +0530

    Add files via upload

---
+++ src/components/JobListings.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import Joblisting from "./Joblisting";
+import Spinner from "./Spinner";
+
+const Joblistings = ({ isHome = false }) => {
+  const [jobs, setjobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setjobs(data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []);
+
+  return (
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          {isHome ? "Recent Jobs" : "All Jobs"}
+        </h2>
+
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <>
+              {jobs.map((job) => (
+                <Joblisting key={job.id} job={job} />
+              ))}
+            </>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Joblistings;
---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Joblistings from "../components/JobListings";
+
+const JobsPage = () => {
+  return (
+    <>
+      <section className="bg-blue-50 px-4 py-6">
+        <Joblistings />
+      </section>
+    </>
+  );
+};
+
+export default JobsPage;
